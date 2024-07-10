a1cf69c2aaadced13a9bbe15f88482c2783f8844
Artur <arturunkulov30@gmail.com>
2024-07-10 14:54:21 +0600

    Added initial files

--- config/settings.py
+++ config/settings.py
@@ -107,17 +107,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'homelife',
-        'USER': 'adminql',
-        'PASSWORD': 'homelife2024',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
