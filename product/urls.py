982608b890df78b641907378c4019c11cf381963
tologonkubandykov <marmokk@icloud.com>
2024-07-17 14:56:40 +0600

    first commit

--- product/urls.py
+++ product/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('colors/<int:pk>/', ColorDetailView.as_view(), name='color-detail'),
     path('list/<int:product_id>', ProductListCreateViewID.as_view(), name='product-list'),
     path('list/', ProductListCreateView.as_view(), name='product-list'),
+    path('list/new/', ProductNewlView.as_view(), name='product-list-new'),
     path('list/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -4,21 +4,26 @@ from .serializers import CategorySerializer, ColorSerializer, ProductSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import ProductFilter
 
+
 class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
 
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+
 class ColorListCreateView(generics.ListCreateAPIView):
     queryset = Color.objects.all()
     serializer_class = ColorSerializer
 
+
 class ColorDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Color.objects.all()
     serializer_class = ColorSerializer
+
 
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -33,10 +38,18 @@ class ProductListCreateViewID(generics.ListCreateAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_class = ProductFilter
 
-
     def get_queryset(self):
         return Product.objects.filter(product_id=self.kwargs["product_id"])
+
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductNewlView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all().order_by('-id')
