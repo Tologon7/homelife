aec2c67c1d3bb393466747729d5240efb97236d0
Artur <arturunkulov30@gmail.com>
2024-09-22 20:39:48 +0600

    rjgiormg

--- config/settings.py
+++ config/settings.py
@@ -65,9 +65,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  # Перенесли выше
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -207,10 +207,10 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 }
 
-
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
+    "http://127.0.0.1:8000",
     "http://homelifee.onrender.com",
 ]
 
