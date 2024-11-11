488fbcd1136cac0f9e75d64136986f901d1dd9bb
Artur <arturunkulov30@gmail.com>
2024-11-11 16:37:20 +0600

    update carrt update price

--- product/models.py
+++ product/models.py
@@ -58,7 +58,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', )
     color = models.ForeignKey(Color, on_delete=models.CASCADE, )
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    promotion = models.DecimalField(max_digits=10, decimal_places=2,)
+    promotion = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='products', )
     quantity = models.IntegerField()
     description = models.TextField(max_length=2551)
